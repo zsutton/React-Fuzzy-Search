e767bbefaf02db27823199fb03d31e9a6ab923f4
Zac Sutton <suppai@gmail.com>
2015-04-23 23:12:52 -0700

    Changed from gulp to webpack

---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+var path = require("path")
+var webpack = require("webpack")
+
+var config = {
+  entry: './src/index.js',
+  output: {
+    filename: 'build/bundle.js',
+    publicPath: "build"
+  },
+  module: {
+    loaders: [
+      { test: /\.js?$/, exclude: /node_modules/, loader: 'babel-loader' },
+    ]
+  },
+  plugins: [ ]
+};
+
+if(process.env.NODE_ENV === 'production') {
+    config.plugins = config.plugins.concat([
+        new webpack.DefinePlugin({
+            "process.env": {
+                NODE_ENV: JSON.stringify("production")
+            }
+        }),
+        new webpack.optimize.DedupePlugin(),
+        new webpack.optimize.UglifyJsPlugin(),
+        new webpack.optimize.OccurenceOrderPlugin()
+    ]);
+}
+else {
+    config.devtool = 'sourcemap';
+    config.debug = true;
+}
+
+module.exports = config;
